96dae573a3027d09dc668f8e35e094dbb72dbab0
njnargis <122391916+njnargis@users.noreply.github.com>
2024-02-27 23:51:19 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Footer(){
+    const year=new Date().getFullYear();
+        return(
+            <footer>Copyright ⓒ {year}</footer>
+        );
+    
+}
+export default Footer;
